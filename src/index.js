64d1484f5b9320cdc4dca4189b7d81bca8552097
viv <l.vivian.93@gmail.com>
2019-02-20 16:53:17 +1100

    Set up app as auto-generated documentation and add first basic HelloWorld component as example.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Docs from './docs/Docs';
 import './index.css';
-import App from './App';
+import '../node_modules/highlight.js/styles/ocean.css'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Docs />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
