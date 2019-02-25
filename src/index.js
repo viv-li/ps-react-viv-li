6fd5a7d6c4aff297f664e3a9003ffe40df20f231
viv <l.vivian.93@gmail.com>
2019-02-25 14:48:38 +1100

    Got enzyme and enzyme adapter set up so can write tests with renderer.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Docs from './docs/Docs';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import './index.css';
 import '../node_modules/highlight.js/styles/ocean.css'
 import * as serviceWorker from './serviceWorker';
 
+configure({ adapter: new Adapter() });
 ReactDOM.render(<Docs />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
